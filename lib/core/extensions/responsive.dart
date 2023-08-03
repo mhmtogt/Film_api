4a7d4d49b7879daf8ae5619a32c7baed0fe38f0e
mehmet <mogut44949@gmail.com>
2023-08-03 12:11:36 +0300

    add_singleton

--- lib/core/extensions/responsive.dart
+++ lib/core/extensions/responsive.dart
@@ -2,30 +2,23 @@ import 'package:flutter/material.dart';
 
 class ScreenSize {
   static final ScreenSize _instance = ScreenSize._init();
- Size screenSize;
 
-  ScreenSize._init() : screenSize = Size(0, 0);
+  ScreenSize._init();
+
+  late Size screenSize;
 
   factory ScreenSize() {
     return _instance;
   }
 
-  void init(BuildContext context) {
-    screenSize = MediaQuery.of(context).size;
-  }
- 
-
   double get deviceWidth => screenSize.width;
   double get deviceHeight => screenSize.height;
 
-  
   double setWidth({required double width}) {
     if (deviceWidth == 0 || deviceHeight == 0) return 0.0;
 
     return screenSize.width / (deviceWidth / width);
   }
-
-  
 
   double setHeight({required double height}) {
     if (deviceHeight == 0 || deviceWidth == 0) return 0.0;
--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,15 @@ import 'package:api_1/core/theme/colors_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
-  
-
 
   @override
   Widget build(BuildContext context) {
-    ScreenSize().init(context);
+    ScreenSize().screenSize = MediaQuery.of(context).size;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'FİLM ',
