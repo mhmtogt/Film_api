f31014ba5aedd5b952200c28d829c85ed8d82b4b
mehmet <mogut44949@gmail.com>
2023-08-21 06:58:30 +0300

    end_point

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -22,6 +22,64 @@ class Api {
     } // internetten gelen veriye göre ok yada değil
   }
 }
+Future<void> postFilm(FilmModel film) async {
+  try {
+    final dio = Dio();
+    dio.options.headers = {'Authorization': Constants.apiKey};
+    
+    // Örnek JSON veri
+    final filmData = film.toJson();
+    
+    final response = await dio.post(_baseUrl + 'post', data: filmData);
+    
+    if (response.statusCode == 200) {
+      print('Film oluşturuldu.');
+    } else {
+      print('POST  Hata kodu: ${response.statusCode}');
+    }
+  } on DioException catch (error) {
+    throw Exception('Bir hata oluştu: $error');
+  }
+}
+
+Future<void> putFilm(FilmModel film) async {
+  try {
+    final dio = Dio();
+    dio.options.headers = {'Authorization': Constants.apiKey};
+    
+    // Örnek JSON veri
+    final filmData = film.toJson();
+    
+    final response = await dio.put(_baseUrl + 'put', data: filmData);
+    
+    if (response.statusCode == 200) {
+      print('Film  güncellendi.');
+    } else {
+      print('PUT Hata kodu: ${response.statusCode}');
+    }
+  } on DioException catch (error) {
+    throw Exception('Bir hata oluştu: $error');
+  }
+}
+
+Future<void> deleteFilm(int filmId) async {
+  try {
+    final dio = Dio();
+    dio.options.headers = {'Authorization': Constants.apiKey};
+    
+    final response = await dio.delete(_baseUrl + 'delete/$filmId');
+    
+    if (response.statusCode == 200) {
+      print('Film  silindi.');
+    } else {
+      print('DELETE Hata kodu: ${response.statusCode}');
+    }
+  } on DioException catch (error) {
+    throw Exception('Bir hata oluştu: $error');
+  }
+}
+
+
 
 
 // GET methodu size bir istek karşılığında geriye veri dönderiyor
